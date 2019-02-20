7e07d13e7b450f1149488d3ad314fd5e21a67d54
elriviere2 <eloi.riviere@etu.uca.fr>
2019-02-20 17:24:01 +0100

    tp3 client

--- tp2/code/Code.java
+++ tp2/code/Code.java
@@ -3,76 +3,203 @@ package code;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.List;
 import java.util.ArrayList;
 
-import objects.Arc;
 import objects.Noeud;
 
 public class Code
 {
-  static int nbNoeuds = 0, nbArcs = 0;
-  
-	public static void main(final String[] args)
-	{ 
-		List<Noeud> listeNoeuds = new ArrayList<>();
-		List<Arc> listeArcs = Code.lecture(args[0]);
-    
-		for(int i=0;i<nbNoeuds;i++)
-		{
-			listeNoeuds.add(new Noeud());
-		}
-	
-		for(Arc arc : listeArcs)
-		{
-			listeNoeuds.get(arc.getEntrant()-1).getListeArcsSortants().add(arc);
-			listeNoeuds.get(arc.getSortant()-1).getListeArcsEntrants().add(arc);				
-		}
-		for(Noeud noeud : listeNoeuds)
-		{
-			System.out.println(noeud);
-		}
-		
-	}
-  
-  public static List<Arc> lecture(String localisation)
-  {
-	  List<Arc> listeArcs;
-	try
-    {
-        
-        File fichier= new File(localisation);
 
-        BufferedReader br = new BufferedReader(new FileReader(fichier));
-        String line= br.readLine();
-        
-        nbNoeuds = Integer.parseInt(line);
-        
-        line= br.readLine();
-        
-        nbArcs = Integer.parseInt(line);
-        
-        listeArcs = new ArrayList<>();
-        
-        int[] indiceArc= new int[nbArcs];
-        int[] indiceDepart= new int[nbArcs];
-        int[] indicArrivee= new int[nbArcs];
-        float[] cout= new float[nbArcs];
-        
-        for(int i=0;i<nbArcs;i++)
+  static Noeud[] liste_noeuds;
+  static int nombre_noeuds;
+  static int nombre_arcs;
+
+
+  public static void main(final String[] args)
+  {
+    Double[][] tableau_distances = tp2_partie1(args[0]);
+    afficher_tableau_distances(tableau_distances);
+    int clients[][] = {{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15},{15,24,30,8,12,18,27,5,42,13,20,6,21,13,19}};
+    afficher_tableau_clients(clients);
+  }
+
+  public static Double[][] tp2_partie1(String fichier)
+  {
+    chargement(fichier);
+
+    Double[][] tableau_distances = new Double[17][17];
+
+    Double[] marques;
+
+    Integer tableau_noeuds[] = {0,145,10, 24, 30, 36, 38, 50, 95, 124, 140, 167, 261, 286, 275, 311,320};
+
+    for(int i=1;i<17;i++)
+    {
+      marques = Dikjstra(tableau_noeuds[i]);
+      for(int j=1;j<17;j++)
+      {
+        tableau_distances[i][j] = (int)(marques[tableau_noeuds[j]] * 100)*1.0/100;
+      }
+    }
+    sauvegarde("tableau.data", tableau_distances);
+    return tableau_distances;
+  }
+
+  public static void chargement(String localisation)
+  {
+    try
+    {
+        BufferedReader buffer = new BufferedReader(new FileReader(new File(localisation)));
+
+        nombre_noeuds = Integer.parseInt(buffer.readLine());
+        nombre_arcs = Integer.parseInt(buffer.readLine());
+
+        liste_noeuds= new Noeud[nombre_noeuds+1];
+
+        for(int i=1;i<=nombre_noeuds;i++)
         {
-          String [] mot = br.readLine().split(";");
-          listeArcs.add(new Arc(Integer.parseInt(mot[0]), Integer.parseInt(mot[1]), Integer.parseInt(mot[2]), Float.parseFloat(mot[3])));         
+          liste_noeuds[i]= new Noeud();
         }
-        
-        br.close();
-        return listeArcs;
+
+        int noeud_entree, noeud_sortie;
+        Double cout;
+
+        for(int i=0;i<nombre_arcs;i++)
+        {
+          String [] mot =buffer.readLine().split(";");
+
+          noeud_entree = Integer.parseInt(mot[1]);
+          noeud_sortie = Integer.parseInt(mot[2]);
+          cout = Double.parseDouble(mot[3]);
+
+          liste_noeuds[noeud_entree].arcs.put(noeud_sortie, cout);
+        }
+        buffer.close();
     }
     catch(Exception e)
     {
-      System.err.println("erreure lecture");
+      System.err.println("Erreur lecture");
     }
-    
-    return null;
   }
+
+  public static Double[] Dikjstra(int noeud_entree)
+  {
+    int i;
+
+    // initialisation de tableau de valeurs d'accès (marques)
+    Double[] marques= new Double[nombre_noeuds+1];
+    for(i=1;i<=nombre_noeuds;i++) {marques[i] = Double.POSITIVE_INFINITY;} marques[noeud_entree] = 0.0;
+
+    // initialisation de la liste des noeuds
+    List<Integer> liste_noeuds_non_traites = new ArrayList<>();
+    for(i=1;i<=nombre_noeuds;i++){liste_noeuds_non_traites.add(i);};
+
+    //traitement dikjstra
+    //tant qu'il reste des neuds non-traités
+    while(!liste_noeuds_non_traites.isEmpty())
+    {
+      // System.out.println("\n\nIl reste des noeuds non-traités !");
+      int num = 0;
+      Double tmp, min = Double.POSITIVE_INFINITY;
+      // je cherche le noeud qui a la marque la plus petite
+      // System.out.println("Je recherche le noeud qui a la marque la plus petite:");
+      for(i=1;i<marques.length;i++)
+      {
+        tmp = marques[i];
+        if(liste_noeuds_non_traites.contains(i) && min > tmp)
+        {
+          min = tmp;
+          num = i;
+        }
+      }
+      // System.out.println("Noeud choisi: " + num);
+      // System.out.println("Marque actuelle du noeud choisi: " + min);
+      // affichage_tableau_marques(marques);
+      //je récupère la liste des sortants de mon noeud
+      for(Integer key : liste_noeuds[num].arcs.keySet())
+      {
+        // pour chacun de mes noeuds je dois verifier que sa marque est inférieure à ma marque + le cout d'accès à ce noeud, sinon je le remplace
+        if(min + liste_noeuds[num].arcs.get(key) < marques[key])
+        {
+          // System.out.println("En passant par " + num + ", le noeud " + key + " aura une marque plus petite. il faut mettre à jour sa marque.");
+          marques[key] = liste_noeuds[num].arcs.get(key) + min;
+        }
+        // else
+        // {
+          // System.out.println("Il n'est pas intéressant de changer la marque de " + key);
+        // }
+      }
+
+      // System.out.println("On a fini de traiter le noeud " + num);
+      liste_noeuds_non_traites.remove((Integer)num);
+    }
+
+    return marques;
+  }
+
+  public static void affichage_tableau_marques(Double marques[]){for(Double d : marques){System.out.println(d);}}
+
+  public static void affichage_liste_noeuds_non_traites(List<Integer> liste_noeuds_non_traites){for(int i : liste_noeuds_non_traites){System.out.println(i);}}
+
+  public static void sauvegarde(String localisation, Double[][]  tableau_distances)
+  {
+    Double tableau_noeuds[] = {145.0,10.0, 24.0, 30.0, 36.0, 38.0, 50.0, 95.0, 124.0, 140.0, 167.0, 261.0, 286.0, 275.0, 311.0, 320.0};
+    int i = 1;
+    for(Double d : tableau_noeuds)
+    {
+      tableau_distances[i][0] = d;
+      tableau_distances[0][i] = d;
+      i++;
+    }
+
+    try
+    {
+
+      FileWriter fw=new FileWriter(localisation);
+
+      for(i=1; i<17; i++)
+      {
+        for(int j=1; j<16; j++)
+        {
+          fw.write(tableau_distances[i][j]+" ");
+        }
+        fw.write(""+tableau_distances[i][16]+"\n");
+      }
+      fw.close();
+    }
+    catch(Exception e){
+      System.err.println("erreure ecriture");
+    }
+  }
+
+  public static void afficher_tableau_clients(int clients[][])
+  {
+    System.out.println("\nTableau de clients:\n");
+    for(int i = 0;i<2;i++)
+    {
+      for(int j=0;j<15;j++)
+      {
+        System.out.print(clients[i][j] + "\t");
+      }
+      System.out.print("\n");
+    }
+  }
+
+  public static void afficher_tableau_distances(Double[][] tableau_distances)
+  {
+    System.out.println("\nTableau de distances:\n");
+    int i, j;
+    for(i=0;i<17;i++)
+    {
+      for(j=0;j<17;j++)
+      {
+        //System.out.println("tableau_distances[" + i + "][" + j + "]: " + tableau_distances[i][j]);
+        System.out.print(tableau_distances[i][j]+"\t");
+      }
+      System.out.println("");
+    }
+  }
+
 }
--- tp2/objects/Noeud.java
+++ tp2/objects/Noeud.java
@@ -1,43 +1,14 @@
 package objects;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Noeud
 {
-	List<Arc> listArcsSortants, listArcsEntrants;
-	
-	public Noeud()
-	{
-		this.listArcsEntrants = new ArrayList<>();
-		this.listArcsSortants = new ArrayList<>();
-	}
-	
-	public List<Arc> getListeArcsSortants()
-	{
-		return listArcsSortants;
-	}
-	
-	public List<Arc> getListeArcsEntrants()
-	{
-		return listArcsEntrants;
-	}
-	
-	@Override
-	public String toString()
-	{
-		String phrase = "";
-		phrase = "Noeud: " + listArcsEntrants.get(0).getSortant() + "\n Liste arcs entrants:\n";
-		for(Arc arc : listArcsEntrants)
-		{
-			phrase += arc;
-		}
-		phrase += "\n Liste arcs sortants:\n";
-		for(Arc arc : listArcsSortants)
-		{
-			phrase += arc;
-		}
-		return phrase + "\n";
-	}
-	
+  public Map<Integer,Double> arcs;
+
+  public Noeud()
+  {
+    arcs = new HashMap<>();
+  }
 }
